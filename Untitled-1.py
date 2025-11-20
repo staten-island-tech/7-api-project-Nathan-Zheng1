d97f59acc2ded881669cc60198fb662afb64493e
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-11-20 07:09:54 -0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,8 +1,8 @@
-""" https://marketstack.com/?utm_source=Github&utm_medium=Referral&utm_campaign=Public-apis-repo-Best-sellers """
+
 import requests
 
-def getPoke(poke):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+def getData(stockdata):
+    response = requests.get(f"https://marketstack.com/?utm_source=Github&utm_medium=Referral&utm_campaign=Public-apis-repo-Best-sellers/{stockdata.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
@@ -15,5 +15,5 @@ def getPoke(poke):
         "types": [t["type"]["name"] for t in data["types"]]
     }
 
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+stock = getData("Nvidia")
+print(stock)
