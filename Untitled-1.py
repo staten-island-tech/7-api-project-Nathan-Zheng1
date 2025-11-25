db437672483b92024bd33b1fe0736328c771fbc7
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-11-25 07:00:35 -0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,14 +1,14 @@
 import requests
 def getData(cryptodata):
-    response = requests.get(f"https://api.coinlore.net/api/ticker/{cryptodata.lower()}")
+    response = requests.get(f"https://api.coinlore.net/api/ticker/?id={cryptodata.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
     return {
-        "name": data["name"]
+        "name" : data["name"]
     }
     
-coin = getData("?id=90")
+coin = getData("90")
 print(coin) 
