9f7d26cf7d5d848d1ba846699360cd1819dcffcb
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-12-02 09:43:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 def getData(cryptodata):
     response = requests.get(f"https://api.coinlore.net/api/ticker/?id={cryptodata.lower()}")
     if response.status_code != 200:
@@ -13,4 +13,23 @@ def getData(cryptodata):
     }
     
 coin = getData("90")
-print(coin)
+print(coin) """
+
+import requests
+
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+pokemon = getPoke("Bulbasaur")
+print(pokemon)
