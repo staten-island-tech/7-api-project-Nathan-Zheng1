d91a2266053f2f9561dc742209bed695614fc803
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-12-05 10:17:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 import requests
 import tkinter as tk
-import json
 def getData(cryptodata):
-    response = requests.get(f"https://api.coinlore.net/api/ticker/?id={cryptodata.lower()}")
+    response = requests.get(f"https://api.coinlore.net/api/tickers/{cryptodata.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
-
-    name = data[0]["name"]
-    symbol = data[0]["symbol"]
-    price_usd = data[0]["price_usd"]
-
-    Bitcoin_data = f"Name: {name}, Symbol: {symbol}, Price (USD): {price_usd}"
-
-    root = tk.Tk()
-    root.title("Crypto Data")
-    root.geometry("1280x780")
 
     def search_function():
         search_request = search.get().lower()
@@ -27,10 +16,31 @@ def getData(cryptodata):
             if search_request == item["name"].lower():
                 result.append(f"{item['name'].lower()}")
 
-        if result == ["bitcoin"]:
-            results_label.config(text=Bitcoin_data)
-        else:
-            results_label.config(text="No matches found.")
+        for i in range(0,99):
+            name = data[i]["name"]
+            symbol = data[i]["symbol"]
+            price_usd = data[i]["price_usd"]
+            percent_change_hour = data[i]["percent_change_1h"]
+            coin_data = f"{name}: Symbol - {symbol}, Price (USD) - {price_usd}, Percent Change (HR) - ({percent_change_hour})"
+            if result == data[i]["name"]:
+                results_label.config(text=coin_data)
+            else:
+                results_label.config(text="No matches found.")
+
+    """ name = data[0]["name"]
+    symbol = data[0]["symbol"]
+    price_usd = data[0]["price_usd"]
+    percent_change_hour = data[0]["percent_change_1h"]
+
+    name2 = data[1]["name"]
+    symbol2 = data[1]["symbol"]
+    price_usd2 = data[1]["price_usd"]
+    percent_change_hour2 = data[1]["percent_change_1h"]
+ """
+    root = tk.Tk()
+    root.title("Crypto Data")
+    root.geometry("1280x780")
+
 
     label = tk.Label(root, text="Welcome to the crypto database! Find information about your most popular types of crypto by searching in the search bar", font=("Times New Roman", 14))
     label.pack(pady=10)
@@ -38,13 +48,13 @@ def getData(cryptodata):
     search = tk.Entry(root, width=30, font=("Aerial", 14))
     search.pack(pady=10)
 
-    search_button = tk.Button(root, text="Search", command=search_function, font=("Arial", 14))
+    search_button = tk.Button(root, text="Search", command=search_function, font=("Times New Roman", 14))
     search_button.pack(pady=5)
 
-    results_label = tk.Label(root, text="", font=("Arial", 14), justify="left")
+    results_label = tk.Label(root, text="", font=("Times New Roman", 14), justify="left")
     results_label.pack(pady=20)
 
     root.mainloop()
     
-coin = getData("90")
+coin = getData("")
 print(coin) 
